535d1bf05e44dc6938e2756a3e569cff4267021f
Elie Guilbaud <elie.guilbaud@sfr.fr>
2021-07-08 15:14:35 +0200

    main

---
+++ main.py
@@ -0,0 +1,4 @@
+from classes.Wallet import *
+from classes.Block import *
+bloc = Block("6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b")
+transaction = bloc.add_transaction("0b024522-cd09-4a4c-a980-ae358c0fbc69", "0fc649af-2948-4925-a2bd-63a5ba485f02", 100)
